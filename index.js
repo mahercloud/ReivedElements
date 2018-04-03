1394f82674bad2f04f33515241c2137612e3b4db
Christopher Maher <chris@maher.cloud>
2018-04-03 18:31:17 +1000

    init

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var elements = express();
 
 var app = require('./../app.json');
 
+var md5 = require('md5');
+
+api.use(validateSignature);
+
 elements.use(function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
@@ -13,3 +17,53 @@ elements.get('/', function(req, res){
 });
 
 elements.listen(app.elements.port, () => console.log('API listening on port ' + app.elements.port))
+
+function createSignature(expire, key){
+    var rand = 'ThisIsTheString';
+
+    var hash = md5(JSON.stringify({
+        rand: rand,
+        expire: expire,
+        secret: keyPairs[key],
+    }));
+
+    return '&rand=' + rand + '&expire=' + expire + '&key=' + key + '&hash=' + hash;
+}
+
+function validateSignature(req, res, next){
+    if(
+        (req.query.rand) && (req.query.expire) && (req.query.hash)
+    ){
+        req.query.expire = parseInt(req.query.expire);
+
+        var hash = md5(JSON.stringify({
+            rand: req.query.rand,
+            expire: req.query.expire,
+            public: app.keys.public,
+            private: app.keys.private
+        }));
+
+        if(hash == req.query.hash){
+            var ts = Math.round((new Date()).getTime() / 1000);
+            if(ts > req.query.expire){
+                res.send({
+                    success: false,
+                    error: 'EXPIRED',
+                    error2: 'EXPIRED: ' + (ts - req.query.expire)
+                });                
+            }else{
+                next();
+            }
+        }else{
+            res.send({
+                success: false,
+                error: 'INVALID HASH'
+            });
+        }
+    }else{
+        res.send({
+            success: false,
+            error: 'MISSING_ARGS'
+        });
+    }
+}
