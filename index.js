650823f22f55efa0ae199e60d31adef60585315a
Christopher Maher <chris@maher.cloud>
2018-04-03 18:21:55 +1000

    init

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var elements = express();
 
-app.use(function(req, res, next){
+var app = require('./../app.json');
+
+elements.use(function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 })
 
-app.get('/', function(req, res){
-    res.send('This is Elements!');
+elements.get('/', function(req, res){
+    elementsres.send('This is Elements!');
 });
 
-app.listen(app.elements.port, () => console.log('API listening on port ' + app.elements.port))
+elements.listen(app.elements.port, () => console.log('API listening on port ' + app.elements.port))
